93c7c6b86cfd599103bca0f0a8632a2ee512c431
fedejimenez <fedej_mdp@hotmail.com>
2019-02-03 22:07:33 -0300

    Add DB configuration

--- config/connection.php
+++ config/connection.php
@@ -8,11 +8,23 @@ class Connect{
 
   protected function connection(){
     try {
-      $connect = $this->dbh = new PDO("mysql:local=localhost;dbname=dbphpsystem", "root", "root");
+      // $connect = $this->dbh = new PDO("mysql:local=localhost;dbname=dbphpsystem", "root", "root");
+      $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+      $server = $url["host"];
+      $username = $url["user"];
+      $password = $url["pass"];
+      $db = substr($url["path"], 1);
+
+      $config = array(
+          'host' => $server ,
+          'user' => $username ,
+          'pw' => $password,
+          'db' => $db 
+      );
       return $connect;
     
     } catch (Exception $e) {
-      print "Error! " . $e->getMessage() . "<br/>"; 
+      print "Error! " . $e->getMessage() . "<br/>";  
       die();
     }
   } // close function connection
